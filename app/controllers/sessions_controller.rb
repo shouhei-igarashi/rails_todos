82d47b91d88dcc4e0eef556650cd378fb246a8aa
shoupei <kongkongjapanese2@gmail.com>
2022-11-09 23:48:53 +0900

    status位置変更とuser_controller追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,34 +13,7 @@ class SessionsController < ApplicationController
 
         token = JWT.encode(payload, rsa_private, "RS256")
         cookies[:token] = token
+        
         render status: :created
-    end
-
-    def show
-        token = cookies[:token]
-        rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('app/auth/service.key')))
-
-        begin
-            decoded_token = JWT.decode(token, rsa_private, true, { algorithm: 'RS256' })
-        rescue JWT::DecodeError, JWT::ExpiredSignature, JWT::VerificationError
-            return render json: { message: 'unauthorized' }, status: :unauthorized
-        end
-
-        user_id = decoded_token.first["sub"]
-
-        # ユーザーを検索
-        user = User.find(user_id)
-
-        # userが取得できた場合はユーザー情報を返す、取得できない場合は認証エラー
-        if user.nil?
-            raise AuthenticationError
-        else
-            render json: {
-            user: {
-                id: user.id,
-                emai: user.email
-            }
-            }, status: :ok
-        end
     end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,14 +9,14 @@ class TodosController < ApplicationController
         todos_create_service = TodosCreateService.new(params[:title], params[:detail], user.id)
         is_success = todos_create_service.create
         
-        render json: { status: is_success ? 200 : 500, title: params[:title], detail:params[:detail]}
+        render json: {title: params[:title], detail:params[:detail]}, status: is_success ? :created : :internal_server_error
     end
 
     def show
         user = get_token_user
 
         todo_search_service = TodosSearchService.new(user.id)
-        render json: { status:200, todos: todo_search_service.find_by_user_id}
+        render json: {todos: todo_search_service.find_by_user_id}, status: :ok
     end
 
     private def get_token_user
